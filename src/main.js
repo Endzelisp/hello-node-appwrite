390144802df346485d67af0dc8731a5c84e9a20e
Armando Endzelis <98135117+Endzelisp@users.noreply.github.com>
2024-10-13 19:26:15 -0400

    update code 01

--- src/main.js
+++ src/main.js
@@ -8,22 +8,30 @@ export default async ({ req, res, log, error }) => {
     .setEndpoint(process.env.APPWRITE_FUNCTION_API_ENDPOINT)
     .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
     .setKey(req.headers['x-appwrite-key'] ?? '');
-  const users = new Users(client);
+  //const users = new Users(client);
 
-  try {
-    const response = await users.list();
+  // try {
+  //   const response = await users.list();
     // Log messages and errors to the Appwrite Console
     // These logs won't be seen by your end users
-    log(`Total users: ${response.total}`);
-  } catch(err) {
-    error("Could not list users: " + err.message);
-  }
+  //   log(`Total users: ${response.total}`);
+  // } catch(err) {
+  //   error("Could not list users: " + err.message);
+  // }
 
   // The req object contains the request data
-  if (req.path === "/ping") {
+  if (req.path === "/add") {
     // Use res object to respond with text(), json(), or binary()
     // Don't forget to return a response!
-    return res.text("Pong");
+    return res.text("I got the route");
+  }
+
+  if (req.path === "/") {
+    // Use res object to respond with text(), json(), or binary()
+    // Don't forget to return a response!
+    return res.send(
+      `<h1>Hello World of Node.js from Appwrite</h1>`
+    );
   }
 
   return res.json({
